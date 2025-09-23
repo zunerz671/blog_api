3ef23b11998c1a4d602bf87f133e94a7a4198e1a
zunerz671 <zunerz671@gmail.com>
2025-09-23 21:01:59 +1000

    added author and article (x2) seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+author1 = Author.find_or_create_by(
+  name: "Author1",
+  age: 35
+)
+
+author2 = Author.find_or_create_by(
+  name: "Author2",
+  age: 40
+)
+
+article1 = Article.find_or_create_by(
+  title: "Article1",
+  content: "Content on Article 1",
+  author_id: 1
+)
+
+article2 = Article.find_or_create_by(
+  title: "Article2",
+  content: "Content on Article 2",
+  author_id: 2
+)
